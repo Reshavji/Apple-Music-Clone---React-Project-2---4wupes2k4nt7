b19ef330ca2e7c247d8af2d65588d383383e601a
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-08-20 16:59:09 +0530

    Add files via upload

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,39 +8,10 @@ import './Sidebar.css';
 
 function Sidebar() {
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      performSearch();
-    }
-  };
-
-  const performSearch = async () => {
-    try {
-      const response = await fetch(`https://academics.newtonschool.co/api/v1/music/song?filter={"title":"${searchQuery}"}`, {
-        headers: {
-          'projectId': 'bmc60xnvc646' 
-        }
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        setSearchResults(data);
-        console.log(data);
-      } else {
-        const errorData = await response.json(); // Try to parse error response
-        console.error('Error performing search:', response.status, errorData);
-      }
-    } catch (error) {
-      console.error('Error performing search:', error);
-    }
-  };
-  
 
   return (
     <div className='sidebar'>
@@ -51,14 +22,16 @@ function Sidebar() {
         <div className='search-icon'>
           <SearchIcon fontSize="small" />
         </div>
-        <input
-          type='text'
-          className="search-field"
-          placeholder='Search...'
-          value={searchQuery}
-          onChange={handleSearchChange}
-          onKeyDown={handleKeyPress}
-        />
+        <Link to={`/radio?search=${encodeURIComponent(searchQuery)}`} onClick={(e) => e.stopPropagation()}>
+  {/* ... */}
+          <input
+            type='text'
+            className="search-field"
+            placeholder='Search...'
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </Link>
       </div>
       <div className='side-nav'>
         <nav>
@@ -85,14 +58,6 @@ function Sidebar() {
             </Link>
           </ul>
         </nav>
-      </div>
-
-      {/* Display search results */}
-      <div className='search-results'>
-        {searchResults.map((result) => (
-          <div key={result.id}>{result.title}</div>
-          // You can customize how you display the search results here
-        ))}
       </div>
     </div>
   );
